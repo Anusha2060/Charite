09f1ffbbc2bcd1a1a18d3b2e65c857568f735b6d
kisan2 <kisanmahat2@gmail.com>
2024-08-07 12:22:52 +0545

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let sidebarshow = document.getElementById("sidebar");
 let Navigation = document.getElementById("navigation");
+let navitems = document.querySelectorAll("#navitems a");
+
 
 
 function Sidebar() {
@@ -10,12 +12,27 @@ function Sidebar() {
   }
 }
 
+
+
 window.addEventListener("scroll", function () {
-  console.log(pageYOffset);
-  if (pageYOffset > 300) {
+  if (pageYOffset > 500) {
 
     Navigation.style.background = "#263B5C";
   } else {
     Navigation.style.backgroundColor = "transparent";
   }
 });
+
+
+
+function Activate(data){
+  console.log(data)
+  navitems.forEach(a=>{
+    if(a.getAttribute('id').toLowerCase()===data.toLowerCase()){ 
+      this.className += " active";
+    }else{
+      this.className += "";
+    }
+    console.log(a.getAttribute('id'))
+  })
+}
